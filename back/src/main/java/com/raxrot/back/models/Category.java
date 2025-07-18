666aeb3c29798c330b8d0f46c07cda397666e12b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-18 20:13:34 +0100

    add builder annotation

--- back/src/main/java/com/raxrot/back/models/Category.java
+++ back/src/main/java/com/raxrot/back/models/Category.java
@@ -1,10 +1,7 @@
 package com.raxrot.back.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
@@ -12,6 +9,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Entity
 @Table(name = "categories")
+@Builder
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
