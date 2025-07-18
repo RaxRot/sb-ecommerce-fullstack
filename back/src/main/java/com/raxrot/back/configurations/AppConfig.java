9fda9f23535f747a59ddcb2616dbd5b5f9f84935
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-18 19:44:34 +0100

    add AppConfig for messages

---
+++ back/src/main/java/com/raxrot/back/configurations/AppConfig.java
@@ -0,0 +1,9 @@
+package com.raxrot.back.configurations;
+
+public class AppConfig {
+    public static final String PAGE_NUMBER="0";
+    public static final String PAGE_SIZE="10";
+    public static final String SORT_BY_ID="id";
+    public static final String SORT_ORDER_ASC="asc";
+    public static final String NO_CATEGORY_FOUND="No category found";
+}
