74fe5069d796420bb75dd7a627fa80e8d8092ca2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-18 21:24:40 +0100

    change CategoryServiceImpl for pagination and sorting

--- back/src/main/java/com/raxrot/back/services/Impl/CategoryServiceImpl.java
+++ back/src/main/java/com/raxrot/back/services/Impl/CategoryServiceImpl.java
@@ -2,12 +2,17 @@ package com.raxrot.back.services.Impl;
 
 import com.raxrot.back.configurations.AppConfig;
 import com.raxrot.back.dtos.CategoryDTO;
+import com.raxrot.back.dtos.CategoryResponse;
 import com.raxrot.back.exceptions.ApiException;
 import com.raxrot.back.models.Category;
 import com.raxrot.back.repoitories.CategoryRepository;
 import com.raxrot.back.services.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,12 +39,35 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public List<CategoryDTO> getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
-        log.info("Fetched {} categories", categories.size());
-        return categories.stream()
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        log.info("Fetching paginated categories - page: {}, size: {}, sortBy: {}, sortOrder: {}",
+                pageNumber, pageSize, sortBy, sortOrder);
+
+        Sort sort = sortOrder.equalsIgnoreCase("desc") ?
+                Sort.by(sortBy).descending() :
+                Sort.by(sortBy).ascending();
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        Page<Category> categoryPage = categoryRepository.findAll(pageable);
+
+        List<CategoryDTO> categoryDTOs = categoryPage.getContent()
+                .stream()
                 .map(category -> modelMapper.map(category, CategoryDTO.class))
                 .toList();
+
+        CategoryResponse response = new CategoryResponse();
+        response.setContent(categoryDTOs);
+        response.setPageNumber(categoryPage.getNumber());
+        response.setPageSize(categoryPage.getSize());
+        response.setTotalElements(categoryPage.getTotalElements());
+        response.setTotalPages(categoryPage.getTotalPages());
+        response.setLastPage(categoryPage.isLast());
+
+        log.info("Page {} of {} fetched. Total elements: {}. Last page: {}",
+                response.getPageNumber(), response.getTotalPages(),
+                response.getTotalElements(), response.isLastPage());
+
+        return response;
     }
 
     @Override
