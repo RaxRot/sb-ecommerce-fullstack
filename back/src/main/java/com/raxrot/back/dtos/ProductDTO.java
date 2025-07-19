07029e094b6d051cb2ed5de4c2374ed16061b018
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 18:55:59 +0100

    add bulder to ProductDTO

--- back/src/main/java/com/raxrot/back/dtos/ProductDTO.java
+++ back/src/main/java/com/raxrot/back/dtos/ProductDTO.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
