3730523ab49fbc1fd0473b6e43ff171f6a4462c4
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 18:48:24 +0100

    add builder to CategoryDTO

--- back/src/main/java/com/raxrot/back/dtos/CategoryDTO.java
+++ back/src/main/java/com/raxrot/back/dtos/CategoryDTO.java
@@ -2,12 +2,14 @@ package com.raxrot.back.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CategoryDTO {
     private Long id;
     @NotBlank(message = "Category name must not be blank")
