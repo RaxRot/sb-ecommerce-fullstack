25afdcb717424108751c61b39530528b3e830437
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 12:29:25 +0100

    add ProductDTO

--- back/src/main/java/com/raxrot/back/repoitories/ProductRepository.java
+++ back/src/main/java/com/raxrot/back/repoitories/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.raxrot.back.repoitories;
 
+import com.raxrot.back.models.Category;
 import com.raxrot.back.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product>findByCategoryOrderByPriceAsc(Category category);
+    List<Product> findByNameLikeIgnoreCase(String keyword);
 }
