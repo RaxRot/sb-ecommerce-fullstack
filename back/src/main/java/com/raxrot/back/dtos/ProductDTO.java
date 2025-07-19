37d9cdd14832353c3cb524529e2b716d0fc12180
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-19 12:29:38 +0100

    add ProductDTO

---
+++ back/src/main/java/com/raxrot/back/dtos/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.raxrot.back.dtos;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+
+    @NotBlank(message = "Product name must not be blank")
+    private String name;
+
+    @NotBlank(message = "Product description must not be blank")
+    private String description;
+
+    private String image;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity must be at least 0")
+    private Integer quantity;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+}
