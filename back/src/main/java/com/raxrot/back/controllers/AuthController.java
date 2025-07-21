f3c54f20b1ae05d78342ac246e3b6300ffbbd71a
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-21 20:06:05 +0100

    add signup

--- back/src/main/java/com/raxrot/back/controllers/AuthController.java
+++ back/src/main/java/com/raxrot/back/controllers/AuthController.java
@@ -1,9 +1,17 @@
 package com.raxrot.back.controllers;
 
+import com.raxrot.back.exceptions.ApiException;
+import com.raxrot.back.models.AppRole;
+import com.raxrot.back.models.Role;
+import com.raxrot.back.models.User;
+import com.raxrot.back.repoitories.RoleRepository;
+import com.raxrot.back.repoitories.UserRepository;
+import com.raxrot.back.security.auth.LoginRequest;
+import com.raxrot.back.security.auth.SignUpRequest;
+import com.raxrot.back.security.auth.UserInfoResponse;
 import com.raxrot.back.security.jwt.JwtUtils;
-import com.raxrot.back.security.jwt.LoginRequest;
-import com.raxrot.back.security.jwt.UserInfoResponse;
 import com.raxrot.back.security.services.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,14 +19,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,9 +34,15 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
-    public AuthController(AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    public AuthController(AuthenticationManager authenticationManager, JwtUtils jwtUtils, UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
     @PostMapping("/signin")
@@ -55,4 +68,46 @@ public class AuthController {
         UserInfoResponse response=new UserInfoResponse(userDetails.getId(),jwtToken,userDetails.getUsername(),roles);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?>registerUser(@Valid @RequestBody SignUpRequest signUpRequest){
+        if (userRepository.existsByUsername(signUpRequest.getUsername())){
+            return new ResponseEntity<>("Username is already in use",HttpStatus.BAD_REQUEST);
+        }
+        if (userRepository.existsByEmail(signUpRequest.getEmail())){
+            return new ResponseEntity<>("Email is already in use",HttpStatus.BAD_REQUEST);
+        }
+
+        User user=new User(
+                signUpRequest.getUsername(),
+                passwordEncoder.encode(signUpRequest.getPassword()),
+                signUpRequest.getEmail()
+        );
+        Set<String>strRoles=signUpRequest.getRoles();
+        Set<Role>roles=new HashSet<>();
+        if(strRoles==null){
+            Role userRole=roleRepository.findByRoleName(AppRole.ROLE_USER).orElseThrow(()->new ApiException("Role not found"));
+            roles.add(userRole);
+        }else{
+            strRoles.forEach(role->{
+                switch (role.toLowerCase()){
+                    case "admin":
+                        Role adminRole=roleRepository.findByRoleName(AppRole.ROLE_ADMIN).orElseThrow(()->new ApiException("Role not found"));
+                        roles.add(adminRole);
+                        break;
+                        case "seller":
+                            Role sellerRole=roleRepository.findByRoleName(AppRole.ROLE_SELLER).orElseThrow(()->new ApiException("Role not found"));
+                            roles.add(sellerRole);
+                            break;
+                            default:
+                                Role userRole=roleRepository.findByRoleName(AppRole.ROLE_USER).orElseThrow(()->new ApiException("Role not found"));
+                                roles.add(userRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
 }
