750c80ab5cad0fc332ce2379bd97a552c47f1139
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-21 20:05:02 +0100

    add SignUpRequest

---
+++ back/src/main/java/com/raxrot/back/security/auth/SignUpRequest.java
@@ -0,0 +1,26 @@
+package com.raxrot.back.security.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Size(min = 3, max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String password;
+
+    private Set<String> roles;
+}
