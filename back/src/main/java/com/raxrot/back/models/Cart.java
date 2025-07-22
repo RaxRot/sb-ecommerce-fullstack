2c67fd6e006d79d0bcf3e66aa399598c80026b0c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-22 12:56:52 +0100

    add Cart and CartItem with relationships

---
+++ back/src/main/java/com/raxrot/back/models/Cart.java
@@ -0,0 +1,30 @@
+package com.raxrot.back.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalPrice=BigDecimal.ZERO;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = {CascadeType.PERSIST,CascadeType.MERGE, CascadeType.REMOVE},orphanRemoval = true)
+    private List<CartItem>cartItems=new ArrayList<>();
+}
