02e4e0e2efe7500bf5f5e5233d6ca821de430b13
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-22 17:14:38 +0100

    add Order,OrderItem,Payment (DTO)

---
+++ back/src/main/java/com/raxrot/back/dtos/OrderItemDTO.java
@@ -0,0 +1,15 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long orderItemId;
+    private ProductDTO product;
+    private Integer quantity;
+    private double orderedProductPrice;
+}
