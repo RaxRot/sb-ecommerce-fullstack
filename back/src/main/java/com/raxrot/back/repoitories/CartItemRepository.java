86c58569e3bd7bf27c20843113ac4979d053ab1f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-22 15:12:06 +0100

    add deleteCartItemByProductIdAndCartId

--- back/src/main/java/com/raxrot/back/repoitories/CartItemRepository.java
+++ back/src/main/java/com/raxrot/back/repoitories/CartItemRepository.java
@@ -1,10 +1,17 @@
 package com.raxrot.back.repoitories;
 
 import com.raxrot.back.models.CartItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     @Query("select ci from CartItem ci where ci.cart.id=?1 and ci.product.id=?2")
     CartItem findCartItemByProductIdAndCartId(Long id, Long productId);
+
+    @Transactional
+    @Modifying
+    @Query("delete from CartItem ci where ci.cart.id=?1 and ci.product.id=?2")
+    void deleteCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
